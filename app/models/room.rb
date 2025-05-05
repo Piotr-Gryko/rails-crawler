7077cc61601c39bfbffadbae48c6edc8d5fc8f14
Piotr Gryko <piotrgryko@outlook.com>
2025-05-05 17:00:32 +0100

    Updated to original dungeon crawler

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,3 +1,22 @@
 class Room < ApplicationRecord
   validates :x, :y, :north_image, :south_image, :east_image, :west_image, presence: true
+
+  # Assuming you have attributes like 'north_image', 'south_image', etc.
+  # These should return the image filenames (e.g., 'north_0_1.jpg')
+
+  def north_image
+    "#{self.id}_north.jpg"
+  end
+
+  def south_image
+    "#{self.id}_south.jpg"
+  end
+
+  def east_image
+    "#{self.id}_east.jpg"
+  end
+
+  def west_image
+    "#{self.id}_west.jpg"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
   root "game#index"
-  post "move", to: "game#move"
-  post "turn", to: "game#turn"
+  post 'move', to: 'game#move'
+  post 'turn', to: 'game#turn'
 end
